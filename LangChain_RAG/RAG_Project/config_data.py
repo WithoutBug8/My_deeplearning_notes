868327cd4841d2f67346b45a39e4f28d430327b7
Bowen <82312965+WithoutBug8@users.noreply.github.com>
2026-07-21 22:00:55 +0800

    Finish  to do the RAG_project

--- LangChain_RAG/RAG_Project/config_data.py
+++ LangChain_RAG/RAG_Project/config_data.py
@@ -1,3 +1,4 @@
+from networkx.algorithms.similarity import simrank_similarity
 
 md5_path = "./data/md5.text"
 
@@ -10,3 +11,17 @@ chunk_size = 1000
 chunk_overlap = 100
 separators = ["\n\n", "\n", ".", "!", "?", "。","！","？"," ",""]
 max_split_char_number = 1000
+
+# 相似度检索的阈值，每次返回匹配的文档数量
+similarity_threshold = 2
+
+# 配置model
+embedding_model_name = "text-embedding-v4"
+chat_model_name = "qwen3-max"
+
+# session状态配置
+session_config = {
+        "configurable": {
+            "session_id": "user_001",
+        }
+}
